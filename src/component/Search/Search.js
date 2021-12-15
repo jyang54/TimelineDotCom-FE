1aa8c990ef977f7f93785c01fab16129ca383ace
SCHJonathan <jonathanyuheng@gmail.com>
2021-12-15 10:44:49 -0600

    [chore] remove unused imports and also reformat some files

--- src/component/Search/Search.js
+++ src/component/Search/Search.js
@@ -1,36 +1,32 @@
-import React, { Component } from "react";
-// import {Button, List, Image, Input,  Container, Search  } from "semantic-ui-react";
-import { Link } from "react-router-dom";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import styles from "./Search.scss";
+import React from "react";
 import "../../normalize.css";
-import { Input } from "@chakra-ui/input";
-import { Button } from "@chakra-ui/button";
-import { useColorModeValue } from "@chakra-ui/color-mode";
-import { Box } from "@chakra-ui/layout";
-// import axios from 'axios'
+import {Input} from "@chakra-ui/input";
+import {Button} from "@chakra-ui/button";
+import {Box} from "@chakra-ui/layout";
 
 function Search() {
-  return (
-    <div>
+    return (
+        <div>
 
-      <Box>
-        <Input></Input>
-        <Button
-          loadingText="Searching"
-          size="lg"
-          bg={"teal.400"}
-          color={"white"}
-          _hover={{
-            bg: "teal.500",
-          }}
-        >
-          {" "}
-          Search
-        </Button>
-      </Box>
-    </div>
-  );
+            <Box>
+                <Input>
+                    Please input here...
+                </Input>
+                <Button
+                    loadingText="Searching"
+                    size="lg"
+                    bg={"teal.400"}
+                    color={"white"}
+                    _hover={{
+                        bg: "teal.500",
+                    }}
+                >
+                    {" "}
+                    Search
+                </Button>
+            </Box>
+        </div>
+    );
 }
 
 export default Search;
--- src/component/User/User.js
+++ src/component/User/User.js
@@ -1,19 +1,15 @@
-import { Avatar } from "@chakra-ui/avatar";
-import { Heading } from "@chakra-ui/layout";
-import React, { Component } from "react";
-// import {Button, List, Image, Input,  Container, Search  } from "semantic-ui-react";
-import { Link } from "react-router-dom";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-// import styles from './Search.scss';
+import {Avatar} from "@chakra-ui/avatar";
+import {Heading} from "@chakra-ui/layout";
+import React from "react";
 import "../../normalize.css";
 
 function User() {
-  return (
-    <div>
-      <Avatar size="xl" name="Jintao Yang"/>
-      <Heading>Jintao Yang</Heading>
-    </div>
-  );
+    return (
+        <div>
+            <Avatar size="xl" name="Jintao Yang"/>
+            <Heading>Jintao Yang</Heading>
+        </div>
+    );
 }
 
 export default User;
